dcd086b93eb4226773b79a1ea514e6df6d93cbc2
Hajime Hoshi <hajimehoshi@gmail.com>
2012-06-07 14:30:33 +0900

    Modified tests

--- server/starchat/handler_test.go
+++ server/starchat/handler_test.go
@@ -9,51 +9,86 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	RequestMethod       string
+	RequestPath         string
+	RequestBody         []byte // []string?
+	ResponseStatusCode  int
+	ResponseContentType string
+	ResponseJSONBody    map[string]string // ?
+}
+
+var testCases = []testCase{
+	{
+		RequestMethod:       "GET",
+		RequestPath:         "/",
+		ResponseStatusCode:  http.StatusOK,
+		ResponseContentType: "text/html; charset=utf-8",
+	},
+	{
+		RequestMethod:      "GET",
+		RequestPath:        "/users/foo/ping",
+		ResponseStatusCode: http.StatusOK,
+	ResponseJSONBody: map[string]string{
+			"result": "pong",
+		},
+	},
+}
+
+func checkTestCase(t *testing.T, rootURL string, tc *testCase) {
+	// TODO: Use RequestBody
+	url := rootURL + tc.RequestPath
+	req, err := http.NewRequest(tc.RequestMethod, url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	{
+		statusCode     := resp.StatusCode
+		wantStatusCode := tc.ResponseStatusCode
+		if statusCode != wantStatusCode {
+			t.Errorf("Status Code = %q; want %q", statusCode, wantStatusCode)
+		}
+	}
+	{
+		contentType     := resp.Header.Get("Content-Type")
+		wantContentType := "application/json; charset=utf-8"
+		if tc.ResponseContentType != "" {
+			wantContentType = tc.ResponseContentType
+		}
+		if contentType != wantContentType {
+			t.Errorf("Content-Type = %q; want %q", contentType, wantContentType)
+		}
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if tc.ResponseJSONBody != nil {
+		v := map[string]interface{}{}
+		if err := json.Unmarshal(body, &v); err != nil {
+			t.Fatal(err)
+		}
+		//t.Log(v)
+		for key, value := range v {
+			wantValue := tc.ResponseJSONBody[key]
+			if value != wantValue {
+				t.Errorf("v[%q] = %q; want %q", key, value, wantValue)
+			}
+		}
+	}
+}
+
 func TestHandler(t *testing.T) {
 	handler := starchat.NewHandler(nil)
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
-	client := &http.Client{}
-	{
-		req, err := http.NewRequest("GET", server.URL + "/", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		resp, err := client.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-		contentType := resp.Header.Get("Content-Type")
-		wantContentType := "text/html; charset=utf-8"
-		if contentType != wantContentType {
-			t.Errorf("Content-Type = %q; want %q", contentType, wantContentType)
-		}
-	}
-	{
-		req, err := http.NewRequest("GET", server.URL + "/users/user1/ping", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		resp, err := client.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-		contentType := resp.Header.Get("Content-Type")
-		wantContentType := "application/json; charset=utf-8"
-		if contentType != wantContentType {
-			t.Errorf("Content-Type = %q; want %q", contentType, wantContentType)
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatal(err)
-		}
-		v := map[string]string{}
-		if err := json.Unmarshal(body, &v); err != nil {
-			t.Fatal(err)
-		}
-		if v["result"] != "pong" {
-			t.Errorf(`v["result"] = %q; want %q`, v["result"], "pong")
-		}
+	for _, testCase := range testCases {
+		checkTestCase(t, server.URL, &testCase)
 	}
 }
